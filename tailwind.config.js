4d6638b252f32820740b9493231840fc3720159d
M.Hassan Amir <hassanamir0506@gmail.com>
2024-09-08 12:41:07 +0500

    added images and also set the break points fro devices in the tailwind cofig fil

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: { min: "360px", max: "639px" },
+        tablet: { min: "640px", max: "1023px" },
+        laptop: { min: "1024px", max: "1279px" },
+        desktop: { min: "1280px", max: "1535px" },
+        "wide-desktop": { min: "1536px" },
+      },
+      colors: {},
+    },
   },
   plugins: [],
 };
