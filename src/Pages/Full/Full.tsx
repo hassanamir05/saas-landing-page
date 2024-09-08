7d1c49fd97372cae7402a4b8440c663b9a9a17bc
M.Hassan Amir <hassanamir0506@gmail.com>
2024-09-08 21:04:12 +0500

    changed displatance between components

--- src/Pages/Full/Full.tsx
+++ src/Pages/Full/Full.tsx
@@ -9,7 +9,7 @@ import {
 
 const Full = () => {
   return (
-    <div className="flex flex-col gap-y-[30px] pb-[30px]">
+    <div className="flex flex-col gap-y-[100px] mobile:gap-y-[30px] pb-[30px]">
       <HeroSection />
       <AboutUs />
       <GetStarted />
